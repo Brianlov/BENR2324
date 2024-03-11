cdce5b49063f7cf99a07c1b89ef2383e4cf1595c
Brian <b022210122@student.utem.edu.my>
2024-03-12 00:23:18 +0800

    Test

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app = express()
+const port=precess.env.PORT||3000;
+
+app.use(express.json())
+
+app.getMaxListeners('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port,()=>{
+    console.log('Example app listening on port ${port}')
+})
+
+
