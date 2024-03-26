7b1a9dbeb56b14aada80a573bb489307fdf31cc6
Brian <brianooi1109@gmail.com>
2024-03-26 10:25:35 +0800

    test4

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ app.listen(port,()=>{
 })
 
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://b022210122:nogizakabrian@benr2423brian1.xdst3gl.mongodb.net/?retryWrites=true&w=majority&appName=benr2423Brian1";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
